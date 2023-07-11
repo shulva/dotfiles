4a7796b48804bb26eff33e307179ed6786b5a841
shulva <1329068252@qq.com>
2023-07-12 00:23:36 +0800

    install

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-files=(.bashrc .vimrc .tmux.conf .zshrc .gitconfig )
+files=(.bashrc .vimrc .tmux.conf .zshrc .gitconfig)
 
-for file in ${files[@]} ;
-	do
-		echo $file
-		ln -sf ~/dotfiles/$file ~/$file
-	done
+for file in ${files[@]}; do
+	echo $file
+	ln -sf ~/dotfiles/$file ~/$file
+done
 
 echo lazyvim
 mkdir -p ~/.config
@@ -15,3 +14,4 @@ ln -sf ~/dotfiles/nvim ~/.config/nvim
 sudo pacman -Sy zsh
 sudo pacman -S zsh-autosuggestions zsh-syntax-highlighting zsh-theme-powerlevel10k zsh-completions
 # if nvim can not copy or use clipboard on windows in wsl,please install win32yank by scoop
+# fonts is CaskaydiaCove Nerd fonts
